be4952803df6cf6fc8eb6d6d69d8e5cbdf95aeb3
Kyle Barnett <kbarnett23@gmail.com>
2018-11-21 22:00:18 -0600

    first commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route, HashRouter } from 'react-router-dom';
+
+export default (
+<HashRouter>
+  <Switch>
+    <Route exact path="/" component={} />
+    <Route path="/charts" component={} />
+    <Route path="/cart" component={} />
+  </Switch>
+</HashRouter>
+)
